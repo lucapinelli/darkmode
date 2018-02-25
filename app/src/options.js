51fc031b9e8db952859a819e4e0ae58fd647101c
Luca Pinelli <pinelli.luca@gmail.com>
2018-02-25 01:29:33 +0100

    refactor

--- app/src/options.js
+++ app/src/options.js
@@ -1,7 +1,24 @@
 (() => {
   class DarkMode {
-    constructor() {
+    constructor(ui) {
       this.darkmodeId = 'darkmode-3eb4-450a-8499-7f77cf2f62aa';
+      this.ui = ui;
+      ui.enable.addEventListener('click', this.enable.bind(this));
+      ui.disable.addEventListener('click', this.disable.bind(this));
+      ui.enableDomain.addEventListener('click', this.enableDomain.bind(this));
+      ui.disableDomain.addEventListener('click', this.disableDomain.bind(this));
+
+      chrome.storage.sync.get(this.darkmodeId, (store) => {
+        const options = store[this.darkmodeId] || {};
+        this.render(options);
+      });
+
+      chrome.storage.onChanged.addListener((changes, area) => {
+        if (!changes[this.darkmodeId] || !changes[this.darkmodeId].newValue) {
+          return;
+        }
+        this.render(changes[this.darkmodeId].newValue);
+      });
     }
 
     updateOptions(updateFn) {
@@ -12,15 +29,19 @@
     }
 
     _enableStyle() {
-      document.body.classList.add(this.darkmodeId);
+      chrome.tabs.executeScript(null, { code: `document.body.classList.add('${this.darkmodeId}')` });
     }
 
     _disableStyle() {
-      document.body.classList.remove(this.darkmodeId);
+      chrome.tabs.executeScript(null, { code: `document.body.classList.remove('${this.darkmodeId}')` });
     }
 
     _getDomain() {
-      return document.location.href.replace(/^[^/]+\/\/([^/]+)\/?.*$/, '$1');
+      return new Promise((resolve, reject) => {
+        chrome.tabs.getSelected(null, (tab) => {
+          resolve(tab.url.replace(/^[^/]+\/\/([^/]+)\/?.*$/, '$1') || 'unknown');
+        });
+      });
     }
 
     enable() {
@@ -35,77 +56,56 @@
 
     enableDomain() {
       this._enableStyle();
-      const domain = this._getDomain();
-      this.updateOptions((options) => ({
-        ...options,
-        site: { ...options.site, [domain]: 'enabled' }
-      }));
+      this._getDomain().then((domain) => {
+        this.updateOptions((options) => ({
+          ...options,
+          site: { ...options.site, [domain]: 'enabled' }
+        }));
+      });
     }
 
     disableDomain() {
       this._disableStyle();
-      const domain = this._getDomain();
-      this.updateOptions((options) => ({
-        ...options,
-        site: { ...options.site, [domain]: 'disabled' }
-      }));
+      this._getDomain().then((domain) => {
+        this.updateOptions((options) => ({
+          ...options,
+          site: { ...options.site, [domain]: 'disabled' }
+        }));
+      });
+    }
+
+    _updateEnableDisable(domEnable, domDisable, enable) {
+      if (enable) {
+        domEnable.innerText = 'Enabled';
+        domEnable.classList.add('selected');
+        domDisable.innerText = 'Disable';
+        domDisable.classList.remove('selected');
+      } else {
+        domEnable.innerText = 'Enable';
+        domEnable.classList.remove('selected');
+        domDisable.innerText = 'Disabled';
+        domDisable.classList.add('selected');
+      }
+    }
+
+    render(options) {
+      this._getDomain().then((domain) => {
+        const dom = this.ui;
+        const isEnabled = options.global !== 'disabled';
+        const isSiteEnabled = isEnabled && options.site[domain] !== 'disabled';
+        this._updateEnableDisable(dom.enable, dom.disable, isEnabled);
+        this._updateEnableDisable(dom.enableDomain, dom.disableDomain, isSiteEnabled);
+      });
     }
   }
 
-  const darkmodeId = 'darkmode-3eb4-450a-8499-7f77cf2f62aa';
-  const dom = {};
-
-  const updateEnableDisable = (domEnable, domDisable, enable) => {
-    if (enable) {
-      domEnable.innerText = 'Enabled';
-      domEnable.classList.add('selected');
-      domDisable.innerText = 'Disable';
-      domDisable.classList.remove('selected');
-    } else {
-      domEnable.innerText = 'Enable';
-      domEnable.classList.remove('selected');
-      domDisable.innerText = 'Disabled';
-      domDisable.classList.add('selected');
-    }
-  };
-
-  const render = (options) => {
-    chrome.tabs.getSelected(null, function(tab) {
-      const domain = tab.url.replace(/^[^/]+\/\/([^/]+)\/?.*$/, '$1') || 'unknown';
-      const isEnabled = options.global !== 'disabled';
-      const isSiteEnabled = isEnabled && options.site[domain] !== 'disabled';
-      updateEnableDisable(dom.enable, dom.disable, isEnabled);
-      updateEnableDisable(dom.enableDomain, dom.disableDomain, isSiteEnabled);
-    });
-  };
-
-  const executeAction = (action) => () => {
-    chrome.tabs.executeScript(null, { code: `(()=>{\n${DarkMode}\nnew DarkMode().${action}\n})()` }, function() {
-      // window.close();
-    });
-  };
-
   document.addEventListener('DOMContentLoaded', (tab) => {
-    dom.enable = document.getElementById('enable');
-    dom.disable = document.getElementById('disable');
-    dom.enableDomain = document.getElementById('enable_domain');
-    dom.disableDomain = document.getElementById('disable_domain');
+    const ui = {};
+    ui.enable = document.getElementById('enable');
+    ui.disable = document.getElementById('disable');
+    ui.enableDomain = document.getElementById('enable_domain');
+    ui.disableDomain = document.getElementById('disable_domain');
 
-    dom.enable.addEventListener('click', executeAction('enable()'));
-    dom.disable.addEventListener('click', executeAction('disable()'));
-    dom.enableDomain.addEventListener('click', executeAction('enableDomain()'));
-    dom.disableDomain.addEventListener('click', executeAction('disableDomain()'));
-
-    chrome.storage.sync.get(darkmodeId, function(store) {
-      const options = store[darkmodeId] || {};
-      render(options);
-    });
-
-    chrome.storage.onChanged.addListener(function(changes, area) {
-      if (!changes[darkmodeId] || !changes[darkmodeId].newValue) {
-        return;
-      }
-      render(changes[darkmodeId].newValue);
-    });
+    new DarkMode(ui); // eslint-disable-line no-new
   });
 })();
